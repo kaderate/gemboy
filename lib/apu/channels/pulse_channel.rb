b1f80a8811a8339011991a472e861727b12a3718
AK <6884306+akettal@users.noreply.github.com>
2026-07-29 17:04:58 +0200

    Extract APU VolumeEnvelope logic to a dedicated class
    To be reused in the future NoiseChannel.

--- lib/apu/channels/pulse_channel.rb
+++ lib/apu/channels/pulse_channel.rb
@@ -2,10 +2,11 @@
 
 require_relative '../period_divider'
 require_relative '../length_timer'
+require_relative '../volume_envelope'
 
 class APU
   # A channel is a sound generator
-  class PulseChannel # rubocop:disable Metrics/ClassLength
+  class PulseChannel
     DUTY_PATTERNS = [
       [0, 0, 0, 0, 0, 0, 0, 1], # 12.5%
       [1, 0, 0, 0, 0, 0, 0, 1], # 25%
@@ -17,7 +18,7 @@ class APU
     LENGTH_TIMER_STEPS = [0, 2, 4, 6].freeze
     FREQUENCY_SWEEP_STEPS = [2, 6].freeze
 
-    attr_reader :channel_number, :volume
+    attr_reader :channel_number
 
     def initialize(channel_number:, mmu:)
       @channel_number = channel_number
@@ -43,10 +44,9 @@ class APU
       # Sound state
       @length_timer = LengthTimer.new(channel_number)
       @period_divider = PeriodDivider.new(channel_number)
-      @volume = 0 # 0..15
+      @volume_envelope = VolumeEnvelope.new
       @duty_cycle = 0 # index in DUTY_PATTERNS (0..3)
       @duty_step = 0 # step in the waveform (0..7)
-      @envelope_sweep_step = 0
       @frequency_sweep_step = 0
       @shadow_frequency = 0
     end
@@ -64,25 +64,24 @@ class APU
       # Period changes only take effect after the current "sample" ends (i.e. the next tick)
       @period_divider.update_next_period_div(fetch_period_div) if registers.key?(@key_nrx3) || registers.key?(@key_nrx4)
       @duty_cycle = fetch_duty_cycle if registers.key?(@key_nrx1)
-      @volume = fetch_volume if registers.key?(@key_nrx2)
+      @volume_envelope.write_volume(fetch_volume) if registers.key?(@key_nrx2)
 
       channel_triggered = registers.key?(@key_nrx4) && registers[@key_nrx4] & 0x80 != 0
       return unless channel_triggered
 
       @enabled = true
       @dac_enabled = fetch_dac_enabled
-      @volume = fetch_volume
       @period_divider.update_current_period_div(fetch_period_div)
       @length_timer.reset(initial_length: @mmu.read(@addr_nrx1) & 0x3F)
+      @volume_envelope.reset(fetch_volume)
       @duty_cycle = fetch_duty_cycle
       @duty_step = 0
-      @envelope_sweep_step = 0
       @frequency_sweep_step = 0
       @shadow_frequency = fetch_frequency
     end
 
     def generate_digital_sample
-      DUTY_PATTERNS.dig(@duty_cycle, @duty_step) * @volume
+      DUTY_PATTERNS.dig(@duty_cycle, @duty_step) * @volume_envelope.volume
     end
 
     def generate_pcm_sample
@@ -105,7 +104,7 @@ class APU
         return unless @frequency_sweep_step >= frequency_sweep_pace
 
         @frequency_sweep_step = 0
-        frequency_sweep_direction = @mmu.read(@addr_nrx1) & 0x08 != 0 ? -1 : 1
+        frequency_sweep_direction = @mmu.read(@addr_nrx1) & 0x08 == 0 ? 1 : -1
         frequency_sweep_shift = @mmu.read(@addr_nrx1) & 0x07
         @shadow_frequency += frequency_sweep_direction * @shadow_frequency / (2**frequency_sweep_shift)
 
@@ -123,19 +122,9 @@ class APU
       # Envelope
       return unless ENVELOPE_STEPS.include?(step)
 
-      @envelope_sweep_step += 1
       envelope_sweep_pace = @mmu.read(@addr_nrx2) & 0x07
-      return if envelope_sweep_pace.zero?
-
-      return unless @envelope_sweep_step >= envelope_sweep_pace
-
-      @envelope_sweep_step = 0
       increment_volume = @mmu.read(@addr_nrx2) & 0x08 != 0
-      if increment_volume
-        @volume += 1 if @volume < 15
-      elsif @volume.positive?
-        @volume -= 1
-      end
+      @volume_envelope.tick(envelope_sweep_pace:, increment_volume:)
     end
 
     def fetch_volume = @mmu.read(@addr_nrx2) >> 4
@@ -143,5 +132,6 @@ class APU
     def fetch_duty_cycle = @mmu.read(@addr_nrx1) >> 6
     def fetch_dac_enabled = @mmu.read(@addr_nrx2) & 0xf8 != 0
     def fetch_frequency = @mmu.read_16(@addr_nrx3) & 0x7FF
+    def volume = @volume_envelope.volume
   end
 end
---
+++ lib/apu/volume_envelope.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+class APU
+  # VolumeEnvelope is a sound generator for the volume envelope channel
+  class VolumeEnvelope
+    attr_reader :volume
+
+    def initialize
+      @envelope_sweep_step = 0
+      @volume = 0
+    end
+
+    def tick(envelope_sweep_pace:, increment_volume:)
+      @envelope_sweep_step += 1
+
+      return if envelope_sweep_pace.zero?
+      return unless @envelope_sweep_step >= envelope_sweep_pace
+
+      @envelope_sweep_step = 0
+      if increment_volume
+        @volume += 1 if @volume < 15
+      elsif @volume.positive?
+        @volume -= 1
+      end
+    end
+
+    def write_volume(vol)
+      @volume = vol
+    end
+
+    def reset(vol)
+      @envelope_sweep_step = 0
+      write_volume(vol)
+    end
+  end
+end
