1d986ca2496e8fedd0fbbb7844699a9c2ccafcc4
AK <6884306+akettal@users.noreply.github.com>
2026-08-05 21:53:58 +0200

    Fix comment

--- lib/mmu.rb
+++ lib/mmu.rb
@@ -443,9 +443,7 @@ class MMU # rubocop:disable Metrics/ClassLength
     (read(ADDR_IE) & read(ADDR_IF)).anybits?(0x1F)
   end
 
-  # Contrairement à pending_interrupts?, ignore IE : utilisé pour le réveil de STOP,
-  # qui sur DMG peut sortir dès qu'une interruption est en attente dans IF, même si
-  # elle n'est pas activée dans IE (comportement matériel distinct de HALT/dispatch normal).
+  # Contrairement à pending_interrupts?, ignore IE (utilisé pour le réveil de STOP).
   def any_interrupt_requested?
     read(ADDR_IF).anybits?(0x1F)
   end
